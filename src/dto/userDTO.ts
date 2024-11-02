a68a41e3e8f51abaa074dd1873643e81e66e6607
Paulo Bahia <pauloipatinga22@gmail.com>
2024-11-01 22:22:34 -0300

    refactor: Refatorar métodos de manipulação de dados no UserDTO.
    - Refatorar métodos de manipulação de dados no UserDTO para utilizar funções auxiliares de forma direta.

--- src/dto/userDTO.ts
+++ src/dto/userDTO.ts
@@ -1,3 +1,5 @@
+import { convertUnixEpochToISO, handleCheckIsPayer, handleStatus, obfuscateEmail } from "../helpers/userHelpers"
+
 export class UserDTO {
   private id: string
   private name: string
@@ -9,10 +11,10 @@ export class UserDTO {
   constructor(user: IUserModel) {
     this.id = user.id;
     this.name = user.name;
-    this.email = this.obfuscateEmail(user.email);
-    this.lastActivity = new Date(user.last_activity).toISOString();
-    this.isPayer = this.handleCheckIsPayer(user.status, user.role)
-    this.status = this.handleStatus(user.status)
+    this.email = obfuscateEmail(user.email);
+    this.lastActivity = convertUnixEpochToISO(user.last_activity);
+    this.isPayer = handleCheckIsPayer(user.status, user.role)
+    this.status = handleStatus(user.status)
   }
 
   public toJSON() {
@@ -24,29 +26,5 @@ export class UserDTO {
       isPayer: this.isPayer,
       status: this.status
     }
-  }
-
-  private handleStatus(status: string): boolean {
-    return status === 'enabled'
-  }
-
-  private handleCheckIsPayer(status: string, role: string): boolean {
-    const isEnable = status == 'enabled';
-    const isSubscriberRole = ['editor', 'admin'].includes(role);
-    return isEnable && isSubscriberRole
-  }
-
-  private obfuscateEmail(email: string): string {
-    const [name, domain] = email.split('@');
-
-    if (domain == 'niuco.com.br') {
-      return email
-    }
-    
-    const nameLenght = name.length
-    
-    const obfuscateEmail = `${name.substring(0, 2)}${"*".repeat(5)}${name.substring(nameLenght - 2)}`
-
-    return `${obfuscateEmail}@${domain}`
   }
 }
